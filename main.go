1698d9bb7e6563cc575aca74c5f172a0475d2a3f
ignashkins <ignashkins@yandex.ru>
2023-02-21 18:21:35 +0300

    updated v2

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package morty
 
 func WhoAreYou() string {
-	return "My name is Morty_2, I'm a crazy teenager."
+	return "My name is Morty.v2, I'm a crazy teenager."
 }
 
 func WhoIsRick() string {
-	return "It's a Сrazy Scientist_2."
+	return "It's a Сrazy Scientist.v2."
 }
 
 func WhereDidYou() string {
-	return "I'm from Rick's Citadel."
+	return "I'm from Rick's Citadel. Only in v2"
 }
